7dfbf6471d926c79dc98b2caa54617711ce29abf
Deltara <50772027+Deltara3@users.noreply.github.com>
2022-05-09 11:58:25 -0400

    Create sexec.sh

---
+++ sexec.sh
@@ -0,0 +1,27 @@
+getlnnum () {
+    TEMP=$(
+        echo "$1" |
+        grep -n "———————————————————————————" 2>&1
+    )
+    if [ $(echo "$TEMP" | wc -l) -eq "2" ]
+    then
+        echo $(echo "$TEMP" | head -n -1 | cut -f1 -d: 2>&1)
+    else
+        echo $(echo "$TEMP" | cut -f1 -d: 2>&1)
+    fi
+}
+
+cat ${@:$#} | tail -n +2 > temp.spwn
+OUT=$(spwn b temp.spwn ${@:1:$#-1} 2>&1)
+
+if [ "$?" -ne 0 ]
+then
+    echo "$OUT"
+    exit
+fi
+
+# Filter output
+TAIL=$(echo "$OUT" | tail -n +$(expr $(getlnnum "$OUT") + 2) 2>&1)
+echo "$TAIL" | head -n -$(expr $(echo "$TAIL" | wc -l) - $(getlnnum "$TAIL") + 1 2>&1)
+
+rm temp.spwn
